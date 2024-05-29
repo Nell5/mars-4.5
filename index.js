ac273e1b6d75834893a72256367eeed8b4a9c17e
Nell5 <neeennever@gmail.com>
2024-05-29 15:36:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// let a = "mars"
+
+// console.log(a.toUpperCase())
+
+// let b = "mars"
+
+// console.log(b.toLowerCase())
+
+// let c = prompt("Ism kiriting")
+
+// console.log(c.trim())
+
+let d = prompt("ismingizni kiriting").toLowerCase()
+
+let e = prompt("harf kiriting").toLowerCase()
+
+if (d.includes(e)) {
+    alert("Bu harf bor")
+} else {
+    alert("Bu harf yoq")
+}
+
